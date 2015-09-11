042bc0fdd8fcc81bed702f27cda5120d5ba9a1de
Yossi Farjoun <farjoun@gmail.com>
2015-09-11 06:58:01 -0400

    make the picard commndline test look nicer

--- src/tests/java/picard/cmdline/PicardCommandLineTest.java
+++ src/tests/java/picard/cmdline/PicardCommandLineTest.java
@@ -12,7 +12,7 @@ public class PicardCommandLineTest {
     @Test
     public void TestPicardPublic() { // this tests fails if any CLP in picard is missing its @CommandLineProgramProperties annotation
         PicardCommandLine picardCommandLine = new PicardCommandLine();
-        picardCommandLine.instanceMain(new String[]{""});
+        picardCommandLine.instanceMain(new String[]{"PicardCommandLine","--help"});
     }
 
 }
