eea5be4d63b511dffaf96ca75c0a7520d2d7d626
Yossi Farjoun <farjoun@gmail.com>
2018-01-15 01:25:55 -0500

    - oops

--- src/main/java/picard/sam/CreateSequenceDictionary.java
+++ src/main/java/picard/sam/CreateSequenceDictionary.java
@@ -24,27 +24,22 @@
 package picard.sam;
 
 import htsjdk.samtools.SAMSequenceDictionary;
-import htsjdk.samtools.SAMSequenceDictionaryCodec;
 import htsjdk.samtools.SAMSequenceRecord;
 import htsjdk.samtools.reference.ReferenceSequence;
 import htsjdk.samtools.reference.ReferenceSequenceFile;
 import htsjdk.samtools.reference.ReferenceSequenceFileFactory;
 import htsjdk.samtools.util.AsciiWriter;
-import htsjdk.samtools.util.CloseableIterator;
-import htsjdk.samtools.util.IOUtil;
 import htsjdk.samtools.util.Log;
 import htsjdk.samtools.util.Md5CalculatingOutputStream;
-import htsjdk.samtools.util.RuntimeIOException;
-import htsjdk.samtools.util.SortingCollection;
 import htsjdk.samtools.util.StringUtil;
 import org.broadinstitute.barclay.argparser.Argument;
+import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
 import org.broadinstitute.barclay.help.DocumentedFeature;
 import picard.PicardException;
 import picard.cmdline.CommandLineProgram;
-import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
+import picard.cmdline.StandardOptionDefinitions;
 import picard.cmdline.argumentcollections.ReferenceArgumentCollection;
 import picard.cmdline.programgroups.ReferenceProgramGroup;
-import picard.cmdline.StandardOptionDefinitions;
 import picard.util.SequenceDictionaryUtils;
 
 import java.io.*;
@@ -52,8 +47,6 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
-
-import static picard.util.SequenceDictionaryUtils.makeSortingCollection;
 
 /**
  * Create a SAM/BAM file from a fasta containing reference sequence. The output SAM file contains a header but no
@@ -269,7 +262,7 @@ public class CreateSequenceDictionary extends CommandLineProgram {
 
         @Override
         public boolean hasNext() {
-            return nextRecord == null;
+            return nextRecord != null;
         }
     }
 }
